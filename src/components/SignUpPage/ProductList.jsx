63a568ae0c3c18ddbbffb1fdc61b48ff5b543a45
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-05-30 20:12:20 +0100

    ft(product page)
    - add product page
    - add page routing

--- src/components/SignUpPage/ProductList.jsx
+++ src/components/SignUpPage/ProductList.jsx
@@ -5,6 +5,12 @@ import React from "react";
  * @desc renders list of products to chose from
  */
 const ProductList = () => {
+  const handleClick = (evt) => {
+    const Cat = evt.target.className;
+
+    let clickEd = {};
+    console.log(evt.target.className.value);
+  };
   return (
     <div className="product-list-page">
       <div className="card-header">
@@ -12,12 +18,13 @@ const ProductList = () => {
         <p className="card-text">Select all that applies</p>
       </div>
       <div className="card-body">
-        <label htmlFor="">
-          <input type="checkbox" />
-          <p>
-            <span></span>
-            <span></span>
-          </p>
+        <label
+          className="product-item"
+          htmlFor="hand-sanitizer"
+          onClick={(evt) => handleClick(evt)}
+        >
+          <span className="category">Hand Sanitizer</span>
+          <span className="sub-category">Sanitizer</span>;
         </label>
         <label htmlFor="">
           <input type="text" />
